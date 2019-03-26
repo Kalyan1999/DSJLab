c47769342f26b62255ff91e41d69885058598a55
nikhiltadikonda <40952012+nikhiltadikonda@users.noreply.github.com>
2019-03-26 20:25:41 +0530

    Rectified mistake of return type in main()

--- 21-12-2018/BoundedExample/BoundedExampleDemo.java
+++ 21-12-2018/BoundedExample/BoundedExampleDemo.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class BoundedExampleDemo
 {
-	public static vopid main(String args[])
+	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
 		
